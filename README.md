b206e906a96b4ecc23cf75abaab7b20e7cfffee1
tsatke <tim.satke@gmail.com>
2021-07-05 16:23:10 +0200

    Create QEMU hello world

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # martim
+
 A ~experimental~ superior kernel written in Rust
 
+## Run in QEMU
+
+```bash
+cargo run
+```
+
+## Build
+
+```bash
+cargo build --target x86_64-martim.json --release
+```
+
 ## Resources
+
 To find a list of resources used for this project, see RESOURCES.md
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+use crate::vga_buffer::{Color, Writer};
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // let vga_buffer = 0xb8000 as *mut u8;
+    //
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+
+    crate::vga_buffer::print_something();
+
+    loop {}
+}
+
+mod vga_buffer;
+
+fn main() {}
