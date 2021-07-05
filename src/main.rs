dd1829a39b3d8c98fe6864725c0e0a5d8ff5dce2
tsatke <tim.satke@gmail.com>
2021-07-05 17:07:58 +0200

    Add println! support and panic handler
    println! prints to the VGA buffer.
    The panic handler prints the panic information using println!.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
+#![feature(panic_info_message)]
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("Panic: '{}' at {}", _info.message().unwrap(), _info.location().unwrap());
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-    //
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
-
-    crate::vga_buffer::print_something();
-
-    loop {}
+    main();
+    panic!("_start returned");
 }
 
-mod vga_buffer;
-
-fn main() {}
+fn main() {
+    println!("Hello, {}!", "World");
+}
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -2,7 +2,17 @@ use core::fmt;
 use core::fmt::Arguments;
 use core::ops::{Deref, DerefMut};
 
+use lazy_static::lazy_static;
+use spin::Mutex;
 use volatile::Volatile;
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -133,17 +143,19 @@ impl Writer {
     }
 }
 
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
 
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!\n");
-    writeln!(writer, "Some {} and some {}!", "text", 42).unwrap();
-    writeln!(writer, "Some {} and some {}!", "text", 42).unwrap();
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
